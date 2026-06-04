e04549b9c13b988fb2834697c1218e33fdb95bfa
Heath Babb <hbabb@techsolvd.com>
2026-06-03 21:16:25 -0400

    Create pages, add marketing layout, and update routes
    - Added `PagesController` with actions for `home`, `about`, `services`, `careers`, `contact`, and `blog`.
    - Created corresponding views for the pages under `app/views/pages`.
    - Introduced `marketing.html.erb` layout for marketing-related pages.
    - Updated routes to include page-specific paths with `root` set to `home`.
    - Removed unused `.idea` IDE configuration files.
    - Updated `Gemfile.lock` to pin `pg` gem version.
    - Minor tweaks to `application.html.erb` layout.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,21 @@
+class PagesController < ApplicationController
+  layout "marketing"
+  s
+  def home
+  end
+
+  def about
+  end
+
+  def services
+  end
+
+  def careers
+  end
+
+  def contact
+  end
+
+  def blog
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root "pages#home"
+
+  get "about", to: "pages#about"
+  get "services", to: "pages#services"
+  get "careers", to: "pages#careers"
+  get "contact", to: "pages#contact"
+  get "blog", to: "pages#blog"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
